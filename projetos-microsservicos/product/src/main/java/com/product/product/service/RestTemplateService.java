2b395b9a26bbdfdf45f1f2c916fbc1c44a40c75c
Leonardonps <leonardonpsilva@gmail.com>
2023-11-23 21:40:08 -0300

    Ajustando url

--- projetos-microsservicos/product/src/main/java/com/product/product/service/RestTemplateService.java
+++ projetos-microsservicos/product/src/main/java/com/product/product/service/RestTemplateService.java
@@ -14,7 +14,7 @@ public class RestTemplateService {
     public ResponseEntity<List<Object>> getRequestSku(String id) {
         RestTemplate restTemplate = new RestTemplate();
         Map<String, String> params = new HashMap<>();
-        ResponseEntity response = restTemplate.getForEntity("http://sku-61iv.onrender.com/getSkubyProduct?id=" + id, Object.class);
+        ResponseEntity response = restTemplate.getForEntity("http://35.160.120.126:8081/getSkubyProduct?id=" + id, Object.class);
         return response;
     }
 }
